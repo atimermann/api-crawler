db8246f9db5d37e42f059b3a4fc783bb10a1d9d6
André Timermann <andre@timermann.com.br>
2024-04-16 18:19:36 -0300

    feat(models): configure relationships and fillable properties
    Update models to define proper relationships and specify fillable fields.

--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -4,8 +4,20 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Currency extends Model
 {
     use HasFactory;
+
+
+    protected $fillable = ['code', 'number', 'decimal', 'name', 'location'];
+
+    /**
+     * Get the locations associated with the currency.
+     */
+    public function locations(): HasMany
+    {
+        return $this->hasMany(Location::class);
+    }
 }
--- app/Models/Location.php
+++ app/Models/Location.php
@@ -8,4 +8,14 @@ use Illuminate\Database\Eloquent\Model;
 class Location extends Model
 {
     use HasFactory;
+
+    protected $fillable = ['currency_id', 'name', 'icon'];
+
+    /**
+     * Get the currency that owns the location.
+     */
+    public function currency()
+    {
+        return $this->belongsTo(Currency::class);
+    }
 }
