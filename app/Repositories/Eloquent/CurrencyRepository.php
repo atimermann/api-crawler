3c862e490a276359f03d2f3bc4160a64bb7ca04a
André Timermann <andre@timermann.com.br>
2024-04-16 18:21:32 -0300

    feat(service): establish CurrencyService for managing data flow
    Implement the CurrencyService to orchestrate the flow of currency data through queries, caching, and crawling operations.

--- app/Repositories/Eloquent/CurrencyRepository.php
+++ app/Repositories/Eloquent/CurrencyRepository.php
@@ -5,17 +5,61 @@ namespace App\Repositories\Eloquent;
 use App\Models\Currency;
 use App\Repositories\Contracts\CurrencyRepositoryInterface;
 
+use Illuminate\Support\Facades\DB;
+
 class CurrencyRepository implements CurrencyRepositoryInterface
 {
-    public function findExistingItems(array $searchItems)
+    public function getCurrenciesByCodeAndNumber(array $codeAndNumberToSearch)
     {
-        return Currency::whereIn('code', $searchItems)
-            ->orWhereIn('number', $searchItems)
-            ->get();
+        return Currency::with(['locations' => function ($query) {
+            $query->select('currency_id', 'name', 'icon');
+        }])
+            ->select('id', 'code', 'number', 'decimal', 'name')  // Inclua 'id' para mapear corretamente os relacionamentos
+            ->get()
+            ->toArray();
+
+    }
+
+
+    public function saveCurrencies($scrappingItems)
+    {
+
+        DB::beginTransaction();
+        try {
+
+            foreach ($scrappingItems as $item) {
+                $this->save($item);
+            }
+            DB::commit();
+        } catch (\Exception $e) {
+            DB::rollback();
+            throw $e;
+        }
+
     }
 
     public function save($item)
     {
-        // TODO: Implement save() method.
+
+        $currency = Currency::create([
+            'code' => $item['code'],
+            'number' => $item['number'],
+            'decimal' => $item['decimal'],
+            'name' => $item['name']
+        ]);
+
+
+        // Atualiza ou cria novas localizações associadas
+        foreach ($item['locations'] as $locationData) {
+            $currency->locations()->create([
+                'name' => $locationData['location'],
+                'icon' => $locationData['flag']
+            ]);
+
+        }
+
+        return $currency;
     }
+
+
 }
