bffe26cd1cac86eb2a69160a5035480707b3df7b
André Timermann <andre@timermann.com.br>
2024-04-16 01:57:34 -0300

    chore(service-provider): enable AppServiceProvider
    Enable the AppServiceProvider within the application.

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,6 @@
 <?php
 
 return [
-    App\Providers\AppCurrencyScraperServiceProvider::class,
+    App\Providers\AppServiceProvider::class,
+    App\Providers\AppCurrencyScraperServiceProvider::class
 ];
